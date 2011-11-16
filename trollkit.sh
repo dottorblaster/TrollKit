76350b5c7c565501d3c5cf71cd864341c87d3818
Alessio <dottorblaster@gmail.com>
2011-11-17 00:03:26 +0100

    More variables. I like entropy.

--- trollkit.sh
+++ trollkit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Define a version number for every part of Netkit
+netkit_ver = 2.8
+netkit_fs_ver = 5.2
+netkit_kernel_ver = 2.8
+
+## Netkit directory
 dir = Netkit
 
 ## Let's create the fuckin' operating directory
