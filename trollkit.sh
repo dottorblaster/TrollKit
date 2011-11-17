aa9fe4ac2b2b20c3f57a30bf3878a3ed37996802
Alessio <dottorblaster@gmail.com>
2011-11-17 19:29:11 +0100

    First alpha

--- trollkit.sh
+++ trollkit.sh
@@ -15,21 +15,23 @@ mkdir ~/${dir}
 cd ~/${dir}
 
 ## Download packages
-wget http://wiki.netkit.org/download/netkit/netkit-${netkit_ver}.tar.bz2
-wget http://wiki.netkit.org/download/netkit-filesystem/netkit-filesystem-i386-F${netkit_fs_ver}.tar.bz2
-wget http://wiki.netkit.org/download/netkit-kernel/netkit-kernel-i386-K${netkit_kernel_ver}.tar.bz2
+wget -c http://wiki.netkit.org/download/netkit/netkit-${netkit_ver}.tar.bz2
+wget -c http://wiki.netkit.org/download/netkit-filesystem/netkit-filesystem-i386-F${netkit_fs_ver}.tar.bz2
+wget -c http://wiki.netkit.org/download/netkit-kernel/netkit-kernel-i386-K${netkit_kernel_ver}.tar.bz2
 
 ## Extracting stuff here. Bloat it out babe!
 for i in $( ls ); do
-	tar -xvjf ${i}.tar.bz2
+	tar -xvjf ${i}
 done
+
+mv netkit/* . && rm -rf netkit
 
 rm -rf *.tar.*
 
 ## Some funny stuff for our .bashrc :>
-echo "export NETKIT_HOME=/home/foo/netkit" > ~/.bashrc
-echo "export MANPATH=:$NETKIT_HOME/man" > ~/.bashrc
-echo "export PATH=$NETKIT_HOME/bin:$PATH" > ~/.bashrc
+echo "export NETKIT_HOME=/home/foo/netkit" >> ~/.bashrc
+echo "export MANPATH=:$NETKIT_HOME/man" >> ~/.bashrc
+echo "export PATH=$NETKIT_HOME/bin:$PATH" >> ~/.bashrc
 
 ## Check!
 ./check_configuration.sh
